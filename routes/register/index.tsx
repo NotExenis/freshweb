cd332f85dfc3a606b2712b6da22e010ca53cc04f
robin <r.oudeluttikhuis@resalepartners.nl>
2024-10-22 11:37:56 +0200

    removal of some files, start back-end

--- routes/register/index.tsx
+++ routes/register/index.tsx
@@ -7,7 +7,7 @@ export default function RegisterPage() {
             <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
               Register an account
             </h1>
-            <form class="space-y-4 md:space-y-6" action="#">
+            <form class="space-y-4 md:space-y-6" action="./codex.ts" method="POST">
               <div>
                 <label
                   for="email"
@@ -56,13 +56,15 @@ export default function RegisterPage() {
                   class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 >
                 </input>
-              </div>                  
-              <button
-                type="submit"
-                class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-              >
-                Register
-              </button>
+              </div>
+              <div class="bg-slate-500 w-50">
+                <button
+                  type="submit"
+                  class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                >
+                  Register
+                </button>
+              </div>
             </form>
           </div>
         </div>
